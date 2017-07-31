910f909004a84d9366c7c3a7b74862066e0a88c5
VishnuVikas K <vikassevishnu@yahoo.com>
2017-07-31 19:49:22 +0530

    Add files via upload

---
+++ InventoryMaintenance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InventoryMaintenance.MvcApplication" Language="C#" %>
---
+++ InventoryMaintenance/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Optimization;
+
+
+namespace InventoryMaintenance
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);            
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new MessageLoggingHandler());
+        }
+    }
+}
