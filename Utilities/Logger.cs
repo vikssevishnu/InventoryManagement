f6f3ce58b944395074daf1d8e844e41478c07604
VishnuVikas K <vikassevishnu@yahoo.com>
2017-07-31 19:56:18 +0530

    Add files via upload

---
+++ Utilities/Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Utilities
+{
+    public class Logger
+    {
+        private static readonly Logger instance = new Logger();
+        private static object obj = new object();
+        private string filePath = string.Empty;
+
+        private Logger()
+        {            
+        }
+        public static Logger Instance()
+        {
+            return instance;
+        }
+
+        public void LogMessage(string logMessage)
+        {            
+            try
+            {
+                lock (obj)
+                {
+                    if (string.IsNullOrEmpty(filePath))
+                        filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationManager.AppSettings["LogFileName"]);
+
+                    using (StreamWriter w = File.AppendText(filePath))
+                    {
+                        Log(logMessage, w);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occured while logging :  " + ex.Message);
+            }
+        }
+
+        private void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("{0} {1}", DateTime.Now.ToString(),
+                    DateTime.Now.ToString());
+                txtWriter.WriteLine("  :{0}", logMessage);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occured while logging :  " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Utilities/ModuleException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Utilities
+{
+    public class ModuleException:Exception
+    {        
+        public long ErrorCode { get; private set; }
+        public string ErrorMessage { get; private set; }
+
+        public ModuleException(long errorCode, string errorMessage)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
